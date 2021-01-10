6b7a3bc3bc2306d45d0e8c36ddea92737bb32805
Mohd Danish <54432454+danishs123@users.noreply.github.com>
2021-01-10 19:23:51 +0530

    Delete danish.tf

--- danish.tf
+++
@@ -1,7 +0,0 @@
-module "mymodule" {
-   source = "git::https://github.com/danishs123/awsterra/blob/main/ec2.tf"
-}
-
-module "danish" {
-   source = "git::https://github.com/danishs123/awsterra/blob/main/s3.tf"
-}
